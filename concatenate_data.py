12dc59944592f2e7d6814fc4acf222161b33ec90
ZaraZ24 <ZaraZ24@lakesideschool.org>
2023-07-15 13:19:52 -0700

    code update

--- concatenate_data.py
+++ concatenate_data.py
@@ -1,2 +1,27 @@
 import pandas as pd
 
+df = pd.read_csv('iButtons_series.csv') # read iButton spreadsheet
+listSites = df['Site'].unique() # get list of sites from spreadsheet
+
+print("SITES")
+for i in listSites:
+    print(i)
+
+site = input("Select a site: ")
+
+if site in listSites:
+    siteRows = df[df['Site'] == site]
+    listTypes = siteRows.loc[siteRows['Type'].notnull(), 'Type'].unique()
+    print("\nTYPES")
+
+    for j in listTypes:
+        print(j)
+
+    type = input("Select a type: ")
+
+    if type in listTypes:
+        print("yay")
+    else:
+        print("Invalid type.")
+else:
+    print("Invalid site name.")
