31d3d93f1eeac6e342ec3ec3c7e9810d60cd7677
ZaraZ24 <ZaraZ24@lakesideschool.org>
2023-07-17 14:27:57 -0700

    too much to describe

--- concatenate_data.py
+++ concatenate_data.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os as os
 
-fileLoc = 'G:/My Drive/School/LSRI/2023_ZaraZ/data/categories/1_buried'
+fileLoc = 'G:/My Drive/School/LSRI/2023_ZaraZ/data/categories/5ridge_buried'
 fileNames = os.listdir(fileLoc)
 # colNames = [c[:-4] for f in fileNames]
 os.chdir(fileLoc)
 
-df = pd.read_csv(fileNames[0], header = 13)
+file = fileNames[0]
+
+df = pd.read_csv(file, header = 13)
 
 # create datetime variable, set as index
 df['datetime'] = pd.to_datetime(df['Date/Time'])
@@ -20,6 +22,7 @@ df.drop(['Unit','Date/Time'],axis = 'columns',inplace = True)
 # plot the temperature  time series
 plt.figure()
 plt.plot(df.index,df.Value)
+plt.title(file)
 plt.xlabel('Time')
 plt.ylabel('Value')
 
