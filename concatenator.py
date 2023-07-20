fb4cb023bc96d48e9f5b813d110f516d9af2978c
ZaraZ24 <ZaraZ24@lakesideschool.org>
2023-07-19 19:22:04 -0700

    concatenated data for analysis

---
+++ concatenator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 19 13:05:54 2023
+
+@author: Zara
+"""
+
+# imports
+import pickle
+import pandas as pd
+import os as os
+
+path = "G:/My Drive/School/LSRI/2023_ZaraZ/data/pkl/" # add data file path here
+site = input("Site: ")
+directory = path + site
+pkl_files = [file for file in os.listdir(directory) if file.endswith(".pkl")]
+
+data = []
+
+# loop through files in site directory
+for file in pkl_files:
+    file_path = os.path.join(directory, file)
+    with open(file_path, 'rb') as f:
+        df = pickle.load(f)        
+        data.append(df)
+
+        # Concatenate all dataframes into one
+        frame = pd.concat(data, ignore_index=True)
+        
+os.chdir("G:/My Drive/School/LSRI/2023_ZaraZ/data/concatenated")
+frame_name = site + '.pkl'
+frame.to_pickle(frame_name) # save as .pkl (pickle) file
+
+print(frame) # for visual confirmation
