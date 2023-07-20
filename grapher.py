cd2a1730a2d36142444021cc90f3b096be483dc0
ZaraZ24 <ZaraZ24@lakesideschool.org>
2023-07-20 08:57:45 -0700

    graphs

--- grapher.py
+++ grapher.py
@@ -7,29 +7,38 @@ Created on Wed Jul 19 19:16:25 2023
 
 #import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import os as os
 import pickle
 
 # IMPORTANT: FILES ARE STRUCTURED DIFFERENTLY
 
-path = 'G:/My Drive/School/LSRI/2023_ZaraZ/data/concatenated/'
-site = input("Site: ")
-directory = path + site + ".pkl"
+directory = 'G:/My Drive/School/LSRI/2023_ZaraZ/data/snow_cover/'
+#site = input("Site: ")
+#directory = path + site + ".pkl"
+sites = [file for file in os.listdir(directory) if file.endswith(".pkl")]
 
 # colNames = [c[:-4] for f in fileNames]
 #os.chdir(fileLoc)
+#plt.title(site)
 
-with open(directory, 'rb') as f:
-    df = pickle.load(f)     
-print(df)    
+plt.figure()
+for site in sites:
+    file_path = os.path.join(directory, site)
+    with open(file_path, 'rb') as f:
+        #print(elevation)
+        df = pickle.load(f)
+        snow_cover = df[(df['Value'] >= -1) & (df['Value'] <= 1)] # change snow cover condition here
+        elevation = int(site[-8:-4]) # gets elevation from file name 
+        elevation_plot = [elevation] * len(snow_cover.index)
+        #print(snow_cover.index)
+        plt.scatter(snow_cover.index, elevation_plot, color = "mediumslateblue")
+
+plt.xlabel('Time')
+plt.ylabel('Elevation (m)')
+plt.ylim(800, 2200)
+
+#print(df)    
 
 # plot the temperature  time series
-plt.figure()
-plt.plot(df.index,df.Value)
-plt.title(site)
-plt.xlabel('Time')
-plt.ylabel('Value')
 
-#plt.figure()
-#plt.plot(df)
+
